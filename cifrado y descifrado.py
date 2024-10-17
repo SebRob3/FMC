47ac5ebbd3592e1379bccb25c3dc68f7b6e6b87e
Sebrob31 <js.roblesm1@uniandes.edu.co>
2024-10-16 19:49:49 -0500

    codigo completado

---
+++ cifrado y descifrado.py
@@ -0,0 +1,82 @@
+def euclides(a, b):
+    qi = []
+    ri = []
+    gcd = None
+    
+    r = b
+    q = a // b
+    mod = a % b
+    ri.append(a)
+    ri.append(b)
+    qi.append(None)
+    qi.append(q)
+    while mod != 0:
+        r, q, mod = mod, r//mod, r % mod
+        ri.append(r)
+        qi.append(q)
+    
+    gcd = ri[-1]
+    return gcd, ri, qi
+
+def extendido_euclides(qi):
+    si = [1,0]
+    ti = [0,1]
+    for i in range(2, len(qi)):
+        si.append(si[i-2] - (qi[i-1]*si[i-1]))
+        ti.append(ti[i-2] - (qi[i-1]*ti[i-1]))
+    
+    return si[-1], ti[-1]
+
+def square_and_multiply(base, exp, mod):
+    bin_exp = list(str(bin(exp)))[2:]
+    result = 1
+    for i in bin_exp:
+        if i == "1":
+            result = ((result**2) * base) % mod
+        else:
+            result = (result**2) % mod
+    
+    return result
+    
+def encrypt_rsa(message, p, q, e):
+    m = p * q
+    phi_m = (p - 1) * (q - 1)
+
+    gcd, ri, qi = euclides(e, phi_m)
+    d, s = extendido_euclides(qi)
+    d += phi_m if d < 0 else 0
+    
+    return square_and_multiply(message, e, m), d
+
+def des_encrypt_rsa(message, p, q, e):
+    phi_m = (p - 1) * (q - 1)
+    
+    gcd, ri, qi = euclides(e, phi_m)
+    d, s = extendido_euclides(qi)
+    d += phi_m if d < 0 else 0
+    
+    return square_and_multiply(message, d, p*q)
+
+p = 83
+q = 997
+e = 5
+message = 100
+c = encrypt_rsa(message, p, q, e)[0]
+d = encrypt_rsa(message, p, q, e)[1]
+print(message, c, d)
+print(des_encrypt_rsa(c, p, q, e))
+
+def encrypt_afin(message, a, b):
+    abcdario = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúÁÉÍÓÚñÑ1234567890.,;: ") 
+    return "".join(list(abcdario[((a*abcdario.index(m))+b)%len(abcdario)] for m in message))
+
+def des_encrypt_afin(message, a, b):
+    abcdario = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúÁÉÍÓÚñÑ1234567890.,;: ") 
+    inverso_a = extendido_euclides(euclides(a, len(abcdario))[2])[0]
+
+    return "".join(list(abcdario[(inverso_a*(abcdario.index(m)-b))%len(abcdario)] for m in message))
+
+message = "Si la gente no cree que las matemáticas son simples, es solo porque no se dan cuentade lo complicado que es la vida"
+encriptacion = encrypt_afin(message, 7, 2)
+print(encriptacion)
+print(des_encrypt_afin(encriptacion, 7, 2))
